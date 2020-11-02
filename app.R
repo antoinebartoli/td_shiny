cff639a31b846d0fdf24a2dcda68d3a83ea031d1
Scrubz00 <younes.chahma@gmail.com>
2020-11-02 12:28:09 +0100

    ajout de la conso et du plot

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 library(shiny)
 library(dplyr)
 library(DT)
+library(shinydashboard)
+library(tidyverse)
 
 
 # Preparation des données -------------------------------------------------
@@ -45,7 +47,9 @@ ui <- navbarPage(
       ##affichage du nom du departement
       h3(textOutput('nom_dep')),
       
-      dataTableOutput('ma_table')
+      dataTableOutput('ma_table'),
+      
+      plotOutput('repartition')
       
     )
     
@@ -95,10 +99,26 @@ server <- function(input, output) {
      select(- contains('superficie'),
             - contains('residences'),
             - contains('taux')
-            ,- contains('geos'))
+            ,- contains('geos')) %>% 
+     select(contains('conso'))
    print(out)
    out
   } )
+   
+  output$repartition <- renderPlot({
+    df_filtre <- filtre() %>% 
+      select(annee,
+             conso_totale_residentiel_mwh_,
+             conso_totale_professionnel_mwh_,
+             conso_totale_agriculture_mwh_,
+             conso_totale_tertiaire_mwh_,
+             conso_totale_autres_mwh_) %>% 
+      pivot_longer(-c("annee"))
+    
+    ggplot(df_filtre) +
+      geom_bar(stat = 'identity') +
+      aes(y = value, x = annee, fill = name)
+  })
   
 }
 
